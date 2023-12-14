6ec635c5de83b9757dc0f95a27cff061c16fa6d1
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-14 19:14:17 -0300

    Ok

--- lSets/Asets.rb
+++ lSets/Asets.rb
@@ -1,17 +1,13 @@
-#El tipo de dato "set", es similar a una lista pero no tiene indices ni ningún tipo de orden.
+#El tipo de dato "set", es similar a un array pero no tiene indices ni ningún tipo de orden.
 
-peliculas: set = {"Django", "Ponyo", "Terminator"}
+peliculas = Set.new(["Django", "Ponyo", "Terminator"])
 
 peliculas.add("Alien") #La función "add" permite añadir un elemento al set (Se agrega en cualquier orden).
 peliculas.add("Alien") #No se agregan datos repetidos.
 peliculas.add("Alien") #No se agregan datos repetidos.
 
-print(peliculas)
+puts peliculas.to_a
 
-peliculas.remove("Alien")
+peliculas.delete("Alien")
 
-print(peliculas)
-
-
-
-
+puts peliculas.to_a

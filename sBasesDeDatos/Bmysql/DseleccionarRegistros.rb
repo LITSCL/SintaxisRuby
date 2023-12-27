37e87d33ed64eec9994a264e5a23e0f8f391795a
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-27 03:37:48 -0300

    Ok

---
+++ sBasesDeDatos/Bmysql/DseleccionarRegistros.rb
@@ -0,0 +1,27 @@
+require 'mysql2' #Este módulo permite trabajar con el DBMS MySQL.
+
+#1. Conectarse a la base de datos (Los parámetros se deben entregar así como variables iniciadas dentro, con los nombres en inglés).
+conexion = Mysql2::Client.new(
+  host: "localhost",
+  username: "root",
+  password: "root",
+  database: "dbpruebas",
+  port: 3306
+)
+
+#3. Obtener todos los registros de la tabla "producto".
+query = conexion.query "SELECT * FROM producto"
+productos = query.to_a
+
+productos.each do |producto|
+  puts producto
+end
+
+puts "-----------------------------------"
+
+productos.each do |producto|
+  puts "ID: #{producto['id']} NOMBRE: #{producto['nombre']} DESCRIPCION: #{producto['descripcion']} PRECIO: #{producto['precio']}"
+end
+
+#4. Cerrar la conexión.
+conexion.close

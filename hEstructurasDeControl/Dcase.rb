bef292203b799ef9c78756adbaa6147937c4f689
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-01-04 19:10:31 -0300

    Ok

---
+++ hEstructurasDeControl/Dcase.rb
@@ -0,0 +1,14 @@
+numero = 10
+
+case numero
+when 7
+  puts "Se ejecutó el primer case"
+when 8
+  puts "Se ejecutó el segundo case"
+when 9
+  puts "Se ejecutó el tercer case"
+when 10
+  puts "Se ejecutó el cuarto case"
+else
+  puts "Se ejecutó el default"
+end

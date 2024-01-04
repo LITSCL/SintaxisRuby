f4014d70882d2cf10e392e56cbfe8a961acae7d8
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2024-01-04 19:12:23 -0300

    Ok

--- hEstructurasDeControl/Dcase.rb
+++ hEstructurasDeControl/Dcase.rb
@@ -2,13 +2,13 @@ numero = 10
 
 case numero
 when 7
-  puts "Se ejecutó el primer case"
+  puts "Se ejecutó el primer when"
 when 8
-  puts "Se ejecutó el segundo case"
+  puts "Se ejecutó el segundo when"
 when 9
-  puts "Se ejecutó el tercer case"
+  puts "Se ejecutó el tercer when"
 when 10
-  puts "Se ejecutó el cuarto case"
+  puts "Se ejecutó el cuarto when"
 else
-  puts "Se ejecutó el default"
+  puts "Se ejecutó el else"
 end
